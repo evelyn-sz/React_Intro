afb35c1aefebb0f280b491f33807f242f2d1f93b
evelyn-sz <evelynszarek@gmail.com>
2021-02-28 15:38:49 +0000

    Book and library constructor functions and prototypal methods replaced with class syntax

---
+++ book.js
@@ -0,0 +1,23 @@
+// const Book = function (author, title, genre) {
+//   this.author = author;
+//   this.title = title;
+//   this.genre = genre;
+// };
+
+class Book {
+  constructor(author, title, genre){
+    this.author = author;
+    this.title = title;
+    this.genre = genre;
+  }
+}
+
+// let Book = class {
+//   constructor(author, title, genre){
+//     this.author = author;
+//     this.title = title;
+//     this.genre = genre;
+//   }
+// }
+
+module.exports = Book;
---
+++ library.js
@@ -0,0 +1,63 @@
+// const Library = function (books = []) {
+//   this.books = books;
+// };
+
+const Book = require("./book");
+
+class Library {
+  constructor(books = []){
+    this.books = books;
+  }
+
+  // Library.prototype.bookCount = function () {
+  //   return this.books.length;
+  // };
+
+get bookCount() {
+  return this.bookCount;
+  }
+
+  bookCount(){
+    return this.books.length;
+  }
+
+// Library.prototype.addBook = function (newBook) {
+//   this.books.push(newBook);
+// };
+
+  addBook(newBook){
+    this.books.push(newBook);
+  }
+
+// Library.prototype.addBooks = function (newBooks) {
+//   newBooks.forEach(book => this.books.push(book));
+// };
+
+  addBooks(newBooks){
+    newBooks.forEach(book => this.books.push(book));
+  }
+
+// Library.prototype.printInventory = function () {
+//   this.books.forEach((book) => {
+//     console.log(`${book.title} by ${book.author}`);
+//   });
+// };
+  printInventory(){
+    this.books.forEach((book) => {
+      console.log(`${book.title} by ${book.author}`);
+    })
+  }
+}
+const books = [
+  new Book('J R R Tolkien', 'The Fellowship of the Ring', 'Fantasy'),
+  new Book('J R R Tolkien', 'The Two Towers', 'Fantasy'),
+  new Book('J R R Tolkien', 'The Return of the King', 'Fantasy')
+];
+
+let library = new Library;
+library.addBooks(books);
+
+library.printInventory();
+// In terminal: node library.js
+
+module.exports = Library;
